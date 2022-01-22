701aed43bd8421493c7ff1a5c282f9faa0578117
Jefter Alves <jefteralves.dev@gmail.com>
2022-01-22 10:53:34 -0300

    Criando o banco de dados utilizando Entity Framework Core, Criando o projeto Web e movendo o contexto do Identity para o projeto Infra.

---
+++ Infra/Configuration/ContextBase.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuration
+{
+    public class ContextBase : IdentityDbContext<AplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=DESKTOP-0GV4J10\\SQLEXPRESS;Initial Catalog=ECOMMERCE;Integrated Security=SSPI;Persist Security Info=False";
+            return strCon;
+        }
+    }
+}
---
+++ Infra/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,94 @@
+﻿using Domain.Interfaces.Generics;
+using Infra.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        public RepositoryGenerics()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        private bool disposedValue;
+
+        public async Task Add(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task Update(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~RepositoryGenerics()
+        // {
+        //     // Não altere este código. Coloque o código de limpeza no método 'Dispose(bool disposing)'
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Não altere este código. Coloque o código de limpeza no método 'Dispose(bool disposing)'
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
